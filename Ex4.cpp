0ff2067372c7b57b1e38862c3ee4d8372a218aba
Nguyễn Thanh Trúc <86333597+thetrucy@users.noreply.github.com>
2025-07-29 16:19:23 +0700

    Update Ex4.cpp

--- Ex4.cpp
+++ Ex4.cpp
@@ -9,14 +9,6 @@ Personnel::Personnel() : fullName(""), dateOfBirth(""), personnelCode("") {}
 Personnel::Personnel(const string& name, const string& dob, const string& code)
     : fullName(name), dateOfBirth(dob), personnelCode(code) {}
 
-void Personnel::displayInfo() const {
-    cout << *this;
-}
-
-void Personnel::inputInfo() {
-    cin >> *this;
-}
-
 string Personnel::getFullName() const {
     return fullName; 
 }
@@ -50,15 +42,6 @@ double Doctor::calculateSalary() const {
     return (yearsOfExperience * 1.5 + weeklyConsultations * 0.5) * 30000;
 }
 
-void Doctor::displaySpecificInfo() const {
-    cout << "Years of Experience: " << yearsOfExperience << endl;
-    cout << "Specialty: " << specialty << endl;
-    cout << "Weekly Consultations: " << weeklyConsultations << endl;
-}
-
-void Doctor::inputInfo() {
-    cin >> *this;
-}
 
 //--------------------------------NURSES-------------------------------------
 
@@ -71,14 +54,6 @@ double Nurse::calculateSalary() const {
     return (dailyPatientCare * 0.8) * 20000;
 }
 
-void Nurse::displaySpecificInfo() const {
-    cout << "Daily patient care Count: " << dailyPatientCare << endl;
-}
-
-void Nurse::inputInfo() {
-    cin >> *this;
-}
-
 //--------------------------------TECHNICIAN-------------------------------------
 
 Technician::Technician() : Personnel(), technicalExperience(0) {}
@@ -89,21 +64,6 @@ Technician::Technician(const string& name, const string& dob, const string& code
 
 double Technician::calculateSalary() const {
     return (equipmentCodes.size() * 2 + technicalExperience) * 25000;
-}
-
-void Technician::displaySpecificInfo() const {
-    cout << "Technical Experience: " << technicalExperience << " years" << endl
-     << "Equipment Count: " << equipmentCodes.size() << endl
-     << "Equipment Codes: ";
-    for (int i = 0; i < equipmentCodes.size(); ++i) {
-        cout << equipmentCodes[i];
-        if (i < equipmentCodes.size() - 1) cout << ", ";
-    }
-    cout << endl;
-}
-
-void Technician::inputInfo() {
-    cin >> *this;
 }
 
 void Technician::addEquipment(const string& equipmentCode) {
@@ -129,14 +89,6 @@ double AdministrativeStaff::calculateSalary() const {
     return (monthlyProcessedDocuments * 0.3 + workExperience * 1.2) * 22000;
 }
 
-void AdministrativeStaff::displaySpecificInfo() const {
-    cout << "Work Experience: " << workExperience << " years\n";
-    cout << "Monthly Processed Documents: " << monthlyProcessedDocuments << endl;
-}
-
-void AdministrativeStaff::inputInfo() {
-    cin >> *this;
-}
 
 
 //------------------Operator Overloading------------------------------
@@ -146,7 +98,6 @@ ostream& operator<<(ostream& os, const Personnel& p) {
     os << "Full Name: " << p.getFullName() << endl;
     os << "Date of Birth: " << p.getDateOfBirth() << endl;
     os << "Personnel Code: " << p.getPersonnelCode() << endl;
-    p.displaySpecificInfo();
     os << "Monthly Salary: " << fixed << setprecision(2) << p.calculateSalary() << endl;
     return os;
 }
@@ -163,7 +114,14 @@ istream& operator>>(istream& is, Personnel& p) {
 }
 
 ostream& operator<<(ostream& os, const Doctor& d) {
-    os << dynamic_cast<const Personnel&>(d);
+    os << "\n=== Doctor Information ===\n";
+    os << "Full Name: " << d.getFullName() << endl;
+    os << "Date of Birth: " << d.getDateOfBirth() << endl;
+    os << "Personnel Code: " << d.getPersonnelCode() << endl;
+    os << "Years of Experience: " << d.yearsOfExperience << endl;
+    os << "Specialty: " << d.specialty << endl;
+    os << "Weekly Consultations: " << d.weeklyConsultations << endl;
+    os << "Monthly Salary: " << fixed << setprecision(2) << d.calculateSalary() << endl;
     return os;
 }
 
@@ -180,7 +138,12 @@ istream& operator>>(istream& is, Doctor& d) {
 }
 
 ostream& operator<<(ostream& os, const Nurse& n) {
-    os << dynamic_cast<const Personnel&>(n);
+    os << "\n=== Nurse Information ===\n";
+    os << "Full Name: " << n.getFullName() << endl;
+    os << "Date of Birth: " << n.getDateOfBirth() << endl;
+    os << "Personnel Code: " << n.getPersonnelCode() << endl;
+    os << "Daily patient care Count: " << n.dailyPatientCare << endl;
+    os << "Monthly Salary: " << fixed << setprecision(2) << n.calculateSalary() << endl;
     return os;
 }
 
@@ -192,7 +155,10 @@ istream& operator>>(istream& is, Nurse& n) {
 }
 
 ostream& operator<<(ostream& os, const Technician& t) {
-    os << dynamic_cast<const Personnel&>(t);
+    os << "\n=== Technician Information ===\n";
+    os << "Full Name: " << t.getFullName() << endl;
+    os << "Date of Birth: " << t.getDateOfBirth() << endl;
+    os << "Personnel Code: " << t.getPersonnelCode() << endl;
     os << "Technical Experience: " << t.technicalExperience << " years" << endl;
     os << "Equipment Count: " << t.equipmentCodes.size() << endl;
     os << "Equipment Codes: ";
@@ -201,6 +167,7 @@ ostream& operator<<(ostream& os, const Technician& t) {
         if (i < t.equipmentCodes.size() - 1) os << ", ";
     }
     os << endl;
+    os << "Monthly Salary: " << fixed << setprecision(2) << t.calculateSalary() << endl;
     return os;
 }
 
@@ -223,7 +190,13 @@ istream& operator>>(istream& is, Technician& t) {
 }
 
 ostream& operator<<(ostream& os, const AdministrativeStaff& a) {
-    os << dynamic_cast<const Personnel&>(a);
+    os << "\n=== Administrative Staff Information ===\n";
+    os << "Full Name: " << a.getFullName() << endl;
+    os << "Date of Birth: " << a.getDateOfBirth() << endl;
+    os << "Personnel Code: " << a.getPersonnelCode() << endl;
+    os << "Work Experience: " << a.workExperience << " years" << endl;
+    os << "Monthly Processed Documents: " << a.monthlyProcessedDocuments << endl;
+    os << "Monthly Salary: " << fixed << setprecision(2) << a.calculateSalary() << endl;
     return os;
 }
 
